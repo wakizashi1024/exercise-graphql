495deedf64f7c7327eb9ee3e91374c625c83c322
wakizashi1024 <wakizashi1024@users.noreply.github.com>
2023-11-25 20:10:03 +0800

    fix source code location error

--- 03-axios-as-client/app.js
+++ 03-axios-as-client/app.js
@@ -3,13 +3,6 @@ const express = require('express')
 const { graphqlHTTP } = require('express-graphql') 
 const cors = require('cors')
 
-// 模擬資料表artiles
-const articles = [
-    { id: 1, title: "First artitle", content: 'Hello World' },
-    { id: 2, title: "Second artitle", content: 'Vanish this world' },
-    { id: 3, title: "Thirdartitle", content: 'Goodbye World' },
-]
-
 // 初始化Express
 const app = express()
 
@@ -18,104 +11,29 @@ app.use(cors())
 
 // 使用GraphQL Schema語法建立schema
 const schema = buildSchema(`
-    type Article {
-        id: ID!,
-        title: String!,
-        content: String!
-        tagList: [String!]
-    }
-
-    # 查詢進入點
     type Query {
-        articles: [Article]
-        article(id: ID!): Article
-    }
-
-    # 參數物件必須以input定義
-    input CreateArticleInput {
-        title: String!
-        content: String!
-        tagList: [String!]
-    }
-    input UpdateArticleInput {
-        title: String!
-        content: String!
-        tagList: [String!]
-    }
-
-    type DeleteStatus {
-        success: Boolean!
-    }
-
-    # 修改進入點
-    type Mutation {
-        createArticle(article: CreateArticleInput): Article
-        updateArticle(id: ID!, article: UpdateArticleInput): Article
-        deleteArticle(id: ID!): DeleteStatus
+        foo: String,
+        count: Int,
     }
 `)
 
 // 定義schma的resolver
 const rootValue = {
-    articles: () => {
-        return articles
+    foo: () => {
+        return "bar";
     },
-    article: ({ id }) => {
-        return articles.find(article => article.id == id)
-    },
-    createArticle: ({ article: createArticle }) => {
-        const nextId = articles.sort((a, b) => a.id - b.id).at(-1).id + 1
-        createArticle.id = nextId
-        articles.push(createArticle)
-
-        return createArticle
-    },
-    updateArticle: ({ id, article: updateArticle }) => {
-        const targetArticleIndex = articles.findIndex(article => article.id == id)
-        if (targetArticleIndex == -1) {
-            return null
-        }
-        else {
-            const targetArticle = articles[targetArticleIndex]
-            targetArticle.title = updateArticle.title ?? targetArticle.title
-            targetArticle.content = updateArticle.content ?? targetArticle.content
-            targetArticle.tagList = updateArticle.tagList ?? targetArticle.tagList
-            
-            articles[targetArticleIndex] = targetArticle
-            return targetArticle
-        }
-    },
-    deleteArticle: ({id}) => {
-        const targetArticleIndex = articles.findIndex(article => article.id == id)
-        if (targetArticleIndex == -1) {
-            return {
-                success: false,
-            }
-        }
-        else {
-            articles.splice(targetArticleIndex, 1)
-        }
-
-        return {
-            success: true,
-        }
+    count: () => {
+        return 666;
     }
 }
 
 // // 查詢schema
-graphql({
-    schema, 
-    source: `{
-        article(id: "2") {
-            id,
-            title,
-            content
-        }
-    }`, 
-    rootValue
-}).then((result) => {
-    console.log(result);
-})
+// graphql({
+//     schema, 
+//     source: "{ count, foo }", 
+//     rootValue}).then((result) => {
+//     console.log(result);
+// })
 
 // 掛載GraphQL中間件
 app.use('/graphql', graphqlHTTP({
